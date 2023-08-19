37da94a8eec8654b675b56a87a64c82bc3d3069b
xrdev <111656745+xrde4@users.noreply.github.com>
2023-08-19 18:17:08 +0200

    Add files via upload

---
+++ src/xrde4/XGameMode/XGameMode.php
@@ -0,0 +1,109 @@
+<?php
+
+/**
+ * GNU LESSER GENERAL PUBLIC LICENSE v3.0
+ 
+██╗░░██╗██████╗░██████╗░███████╗██╗░░░██╗
+╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██║░░░██║
+░╚███╔╝░██████╔╝██║░░██║█████╗░░╚██╗░██╔╝
+░██╔██╗░██╔══██╗██║░░██║██╔══╝░░░╚████╔╝░
+██╔╝╚██╗██║░░██║██████╔╝███████╗░░╚██╔╝░░
+╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚══════╝░░░╚═╝░░░
+ 
+ If you find my plugin helpful, could you please consider giving it a star on my profile? 
+ Your support means a lot to me! 🌟 Thank you! (https://github.com/xrde4)"
+ */
+ 
+declare(strict_types=1);
+ 
+namespace xrde4\XGameMode;
+
+use pocketmine\Server;
+use pocketmine\utils\Config;
+use pocketmine\nbt\NBT;
+use pocketmine\nbt\tag\CompoundTag;
+use pocketmine\nbt\tag\ListTag;
+use pocketmine\nbt\TreeRoot;
+use pocketmine\data\SavedDataLoadingException;
+use pocketmine\item\Item;
+use xrde4\XGameMode\command\XGameModeCommand;
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+use pocketmine\nbt\BigEndianNbtSerializer;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\network\mcpe\protocol\GameRulesChangedPacket;
+use pocketmine\network\mcpe\protocol\types\BoolGameRule;
+use pocketmine\item\{StringToItemParser, LegacyStringToItemParser};
+use function zlib_decode;
+use function zlib_encode;
+use const ZLIB_ENCODING_GZIP;
+
+class XGameMode extends PluginBase implements Listener
+{
+	
+	private const TAG_NAME = "contents";
+		
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->users = new Config($this->getDataFolder()."config.yml", Config::YAML,[
+		    "language" => "ENG",
+			"change_gm_message_ua" => "§f§l§o[§c!§f] §8§l§o§fG§eM§r§8§7 :: §fВи успішно §aзмінили §fрежим гри.",
+			"no_permissions_ua" => "§f§l§o[§c!§f] §8§l§o§fG§eM§r§8§7 :: §fУ вас §cнедостатньо §fправ для використання цієї команди.",
+			"description_ua" => "Перемкнути режим гри.",			
+			"change_gm_message" => "§f§l§o[§c!§f] §8§l§o§fX§eC§r§8§7 :: §fYou §asuccessfully §fchanged §fgamemode.",
+			"no_permissions" => "§f§l§o[§c!§f] §8§l§o§fG§eM§r§8§7 :: §fYou not have permissions!",
+			"description" => "Change game mode.."]);
+		if (!file_exists($this->getDataFolder() . "Players")) {
+            mkdir($this->getDataFolder() . "Players");
+        }
+	   	 $this->getServer()->getCommandMap()->register("XGameMode", new XGameModeCommand($this));
+    }
+	
+	
+ 	public function serialize(array $contents) : string{
+		if(count($contents) === 0){
+			return "";
+		}
+
+		$contents_tag = [];
+		foreach($contents as $slot => $item){
+			$contents_tag[] = $item->nbtSerialize($slot);
+		}
+		return (new BigEndianNbtSerializer())->write(new TreeRoot(CompoundTag::create()->setTag(self::TAG_NAME, new ListTag($contents_tag, NBT::TAG_Compound))));
+	}
+	
+	public function deSerialize(string $string) : array{
+		if($string == ""){
+			return [];
+		}
+
+		$tag = (new BigEndianNbtSerializer())->read($string)->mustGetCompoundTag()->getListTag(self::TAG_NAME) ?? throw new InvalidArgumentException("Invalid serialized string specified");
+
+		$contents = [];
+		foreach($tag as $value){
+			try{ 
+				$item = Item::nbtDeserialize($value);
+			}catch(SavedDataLoadingException){
+				continue;
+			}
+			$contents[$value->getByte("Slot")] = $item;
+		}
+		return $contents;
+	} 
+	
+
+	public function LanguageMessage($message){
+		if($this->users->getNested("language") == "UA"){
+			if($message == "change_gm_message"){
+				return $this->users->getNested("change_gm_message_ua");
+			}elseif($message == "no_permissions"){	
+				return $this->users->getNested("no_permissions_ua");
+			}elseif($message == "description"){	
+				return $this->users->getNested("description_ua");				
+			}
+		}else{
+			return $this->users->getNested($message);
+		}
+	}
+}
+	
---
+++ src/xrde4/XGameMode/command/XGameModeCommand.php
@@ -0,0 +1,131 @@
+<?php
+
+declare(strict_types=1);
+
+namespace xrde4\XGameMode\command;
+
+use pocketmine\Server;
+use xrde4\XGameMode\XGameMode;
+use pocketmine\plugin\Plugin;
+use pocketmine\plugin\PluginOwned;
+use pocketmine\plugin\PluginOwnedTrait;
+use pocketmine\plugin\PluginBase;
+use pocketmine\player\Player;
+use pocketmine\utils\Config;
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\network\mcpe\protocol\GameRulesChangedPacket;
+use pocketmine\network\mcpe\protocol\types\BoolGameRule;
+use pocketmine\player\GameMode;
+
+class XGameModeCommand extends Command implements PluginOwned{
+		use PluginOwnedTrait {
+		__construct as setOwningPlugin;
+	}
+	
+	public function __construct(XGameMode $plugin){
+        $this->setOwningPlugin($plugin);
+		parent::__construct("gm", $plugin->LanguageMessage("description"), "/gm");
+	 	$this->setPermission('xgamemode.perms');
+		$this->plugin = $plugin;
+	}
+
+	public function execute(CommandSender $sender, string $commandLabel, array $args){
+
+		if (!$sender->hasPermission('xgamemode.perms')) {
+            $sender->sendMessage($this->plugin->LanguageMessage("no_permissions"));
+            return FALSE;
+        }	
+
+        if(!$sender instanceof Player){
+        	$sender->sendMessage("§f§l§o[§c!§f] §8§l§o§fG§eM§r§8§7 :: §c Exclusively in the game.");
+        	return;
+        }
+		
+        if (!file_exists($this->plugin->getDataFolder() . "Players/" . $sender->getName() . ".yml")) {
+            new Config($this->plugin->getDataFolder() . "Players/" . $sender->getName() . ".yml", Config::YAML, array(
+                "GAMEMODE" => $sender->getGamemode()->getEnglishName(),
+				"ARMOR_INVENTORY" => null,
+				"OFFHAND_INVENTORY" => null,
+				"INVENTORY" => null
+            ));
+        }
+		$playerInventory = new Config($this->plugin->getDataFolder() . "Players/" . $sender->getName() . ".yml", Config::YAML);
+		
+		if ($sender->getGamemode() === GameMode::SURVIVAL()) {
+			if($sender->getGamemode()->getEnglishName() != $playerInventory->get("GAMEMODE")){
+				$sender->sendMessage("§f§l§o[§c!§f] §8§l§o§fG§eM§r§8§7 :: §fYour mode §cdoes not match §fthe game mode of the last modification.");
+			return;
+			}
+        $invContent = $sender->getInventory()->getContents();
+		$armorContent = $sender->getArmorInventory()->getContents();
+		$offhandInventory = $sender->getoffHandInventory()->getContents();
+
+		$inv64 = $this->plugin->serialize($invContent);
+		$armor64 = $this->plugin->serialize($armorContent); 
+		$offhand64 = $this->plugin->serialize($offhandInventory); 
+		
+		$sender->getInventory()->clearAll();
+		$sender->getArmorInventory()->clearAll();
+		$sender->getoffHandInventory()->clearAll();
+		$sender->setGamemode(GameMode::CREATIVE());
+		
+		if(is_string($playerInventory->get("INVENTORY"))){
+		$sender->getInventory()->setContents($this->plugin->deSerialize(base64_decode($playerInventory->get("INVENTORY"))));
+		}
+		if(is_string($playerInventory->get("ARMOR_INVENTORY"))){
+		$sender->getArmorInventory()->setContents($this->plugin->deSerialize(base64_decode($playerInventory->get("ARMOR_INVENTORY"))));
+		}
+     	if(is_string($playerInventory->get("OFFHAND_INVENTORY"))){
+		$sender->getOffHandInventory()->setContents($this->plugin->deSerialize(base64_decode($playerInventory->get("OFFHAND_INVENTORY"))));
+		} 
+		
+		$playerInventory->setNested("ARMOR_INVENTORY", base64_encode($armor64));
+		$playerInventory->setNested("INVENTORY", base64_encode($inv64));
+		$playerInventory->setNested("OFFHAND_INVENTORY", base64_encode($offhand64));
+		$playerInventory->setNested("GAMEMODE", $sender->getGamemode()->getEnglishName());
+		$playerInventory->save();
+		$sender->sendMessage($this->plugin->LanguageMessage("change_gm_message"));
+		return;
+		
+	}elseif($sender->getGamemode() === GameMode::CREATIVE()) {
+        if($sender->getGamemode()->getEnglishName() != $playerInventory->get("GAMEMODE")){
+			$sender->sendMessage("§f§l§o[§c!§f] §8§l§o§fG§eM§r§8§7 :: §fYour mode §cdoes not match §fthe game mode of the last modification.");
+		 return;
+		 }
+        $invContent = $sender->getInventory()->getContents();
+		$armorContent = $sender->getArmorInventory()->getContents();
+		$offhandInventory = $sender->getoffHandInventory()->getContents();
+
+		$inv64 = $this->plugin->serialize($invContent);
+		$armor64 = $this->plugin->serialize($armorContent); 
+		$offhand64 = $this->plugin->serialize($offhandInventory); 
+		
+		$sender->getInventory()->clearAll();
+		$sender->getArmorInventory()->clearAll();
+		$sender->getoffHandInventory()->clearAll();
+		$sender->setGamemode(GameMode::SURVIVAL());
+		
+		if(is_string($playerInventory->get("INVENTORY"))){
+		$sender->getInventory()->setContents($this->plugin->deSerialize(base64_decode($playerInventory->get("INVENTORY"))));
+		}
+		if(is_string($playerInventory->get("ARMOR_INVENTORY"))){
+		$sender->getArmorInventory()->setContents($this->plugin->deSerialize(base64_decode($playerInventory->get("ARMOR_INVENTORY"))));
+		}
+		if(is_string($playerInventory->get("OFFHAND_INVENTORY"))){
+		$sender->getOffHandInventory()->setContents($this->plugin->deSerialize(base64_decode($playerInventory->get("OFFHAND_INVENTORY"))));
+		} 
+		
+		$playerInventory->setNested("ARMOR_INVENTORY", base64_encode($armor64));
+		$playerInventory->setNested("INVENTORY", base64_encode($inv64));
+		$playerInventory->setNested("OFFHAND_INVENTORY", base64_encode($offhand64));
+		$playerInventory->setNested("GAMEMODE", $sender->getGamemode()->getEnglishName());
+		$playerInventory->save();		
+		$sender->sendMessage($this->plugin->LanguageMessage("change_gm_message"));
+		return;
+}else{
+				$sender->sendMessage("§f§l§o[§c!§f] §8§l§o§fG§eM§r§8§7 :: §fYour mode §cdoes not match §fany of the available modes");
+		 return;
+}
+	}
+}
+		
